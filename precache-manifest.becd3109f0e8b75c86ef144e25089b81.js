91b95caf54cf24517b5f5a06e68835a559482000
Ryan Tong <rrtong@ucdavis.edu>
2019-03-13 22:19:54 -0700

    replacing old website with new preliminary website

---
+++ precache-manifest.becd3109f0e8b75c86ef144e25089b81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7a9693f5f1b7328f015d24e1aa4b425",
+    "url": "/static/media/rrtong_resume.a7a9693f.pdf"
+  },
+  {
+    "revision": "0b103a0e79706a1ee08268a907216ea1",
+    "url": "/static/media/githubW.0b103a0e.png"
+  },
+  {
+    "revision": "7e5af14a92d57333c2581ded49f780db",
+    "url": "/static/media/githubFilled.7e5af14a.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "41e757515b9774d6bf32",
+    "url": "/static/js/main.41e75751.chunk.js"
+  },
+  {
+    "revision": "6d81eb190309684173f5",
+    "url": "/static/js/2.6d81eb19.chunk.js"
+  },
+  {
+    "revision": "41e757515b9774d6bf32",
+    "url": "/static/css/main.fc383fe0.chunk.css"
+  },
+  {
+    "revision": "5eae4b77ca4b54f303feb62223c34919",
+    "url": "/index.html"
+  }
+];
