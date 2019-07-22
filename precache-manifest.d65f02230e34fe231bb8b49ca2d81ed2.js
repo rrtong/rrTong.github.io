ce2dff6f3f15c36466dc3ab5f0661db5b717b8ae
Ryan Tong <rrtong@ucdavis.edu>
2019-07-21 22:26:09 -0700

    make about page toggleable

--- precache-manifest.d65f02230e34fe231bb8b49ca2d81ed2.js
+++ precache-manifest.d65f02230e34fe231bb8b49ca2d81ed2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.42ac5946.js"
   },
   {
-    "revision": "8fe6ed7da0054da2391d",
-    "url": "/static/js/main.8fe6ed7d.chunk.js"
+    "revision": "f000c211d90453c7a69e",
+    "url": "/static/js/main.f000c211.chunk.js"
   },
   {
     "revision": "c66b79e8e804f9ff899a",
     "url": "/static/js/2.c66b79e8.chunk.js"
   },
   {
-    "revision": "8fe6ed7da0054da2391d",
+    "revision": "f000c211d90453c7a69e",
     "url": "/static/css/main.bec3077d.chunk.css"
   },
   {
-    "revision": "4ed46f8bed6b853b8ea0f973c0ac8f73",
+    "revision": "6989232000a9ea8ecd873788bd568455",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.15322d9ceb3c94f0c3d12703f8c21df0.js"
+  "/precache-manifest.d65f02230e34fe231bb8b49ca2d81ed2.js"
 );
 
 workbox.clientsClaim();
